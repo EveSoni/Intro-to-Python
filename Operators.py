e7bf1a72cb0a93e8e2f992dbb06f44eac189d708
EveSoni <mevelyne051@gmail.com>
2023-10-19 12:59:32 +0300

    addittional file

---
+++ Operators.py
@@ -0,0 +1,39 @@
+# ARITHMETIC OPERATORS
+num=15
+num2=17
+print(f"sum is  {num + num2}")
+print(f"diff {num2 - num}")
+print(f"product {num * num2}")
+print(f"quotient {num/num2}")
+print(f"remainder {num2%num}")
+
+# COMPARISON OPERATORS
+x=2
+y=8
+print(f"two numbers are equal or not {x==y}")
+print(f"two numbers are not equal or not {x!=y}")
+print(f"{x} is less than or equal to {y} {x<=y}")
+print(f"y is greater than or equal to x {y>=x}")
+print(f"{x} is less than {y} {x<y}")
+print(f"{y} is greater than {x} {y>x}")
+
+# ASSIGNMENT OPERATORS
+e=24
+n=8
+print(f"e=n: {e==n}")
+print(f"e+=n: {e+n}")
+print(f"e-=n: {e-n}")
+print(f"e*=n: {e*n}")
+print(f"e/=n: {e/n}")
+print(f"e%=n: {e%n}")
+print(f"e**=n: {e**n}")
+print(f"e//=n: {e//n}")
+
+# LOGICAL OPERATORS
+x=7
+print(f"Is this statement true: {x<10 and x>7}")
+print(f"Any one statement is true: {x<10 or x>7}")
+print(f"Each statement is true then return false or vice-versa: {not{x<10 and x>7}}")
+
+
+
